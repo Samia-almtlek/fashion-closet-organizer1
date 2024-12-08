b169b5791e2454b2ecaba8c6003b3bada5084cc3
Samia-almtlek <samia.almoutlak@gmail.com>
2024-12-08 23:08:22 +0100

    Add test cases for Fashion Closet Organizer application

---
+++ src/test/java/com/fashioncloset/fashion_closet_organizer1/FashionClosetOrganizer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fashioncloset.fashion_closet_organizer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionClosetOrganizer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
