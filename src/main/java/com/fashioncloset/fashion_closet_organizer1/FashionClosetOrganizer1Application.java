bbf0bf0e1bdf9c768900172889cc1ba916bcc44c
Samia-almtlek <samia.almoutlak@gmail.com>
2024-12-08 23:08:09 +0100

    Initialize main application class for Fashion Closet Organizer

---
+++ src/main/java/com/fashioncloset/fashion_closet_organizer1/FashionClosetOrganizer1Application.java
@@ -0,0 +1,15 @@
+package com.fashioncloset.fashion_closet_organizer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class FashionClosetOrganizer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionClosetOrganizer1Application.class, args);
+	}
+
+}
