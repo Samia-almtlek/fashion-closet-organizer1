22dde85586df14f6c5f218a6c406d998f66e7e2c
Samia-almtlek <samia.almoutlak@gmail.com>
2024-12-08 23:11:04 +0100

    Implement User entity for user data management

---
+++ src/main/java/com/fashioncloset/fashion_closet_organizer1/model/User.java
@@ -0,0 +1,30 @@
+package com.fashioncloset.fashion_closet_organizer1.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ClothingItem> clothingItems;
+
+    public void setRole(String roleUser) {
+    }
+}
